0474690a101d0b0a4cfd694ba7dadca1929c89b7
Yusuf Sindi <yysindi@ymail.com>
2021-10-15 15:15:28 +0100

    fixed br line

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,8 @@ export default function About() {
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Yusuf Sindi
-            <br className="hidden lg:inline-block" />
+            <br />
+            {/* <br className="hidden lg:inline-block" /> */}
             Full-Stack Developer
           </h1>
           <p className="mb-8 leading-relaxed">
